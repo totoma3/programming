9d1bc2a30ca0d2e036f61a1883912acf061d843e
totoma3 <totoma3@naver.com>
2021-05-07 20:45:49 +0900

    Create pythonfor.py

---
+++ pythonfor.py
@@ -0,0 +1,16 @@
+#for irum in "안녕하 세요":
+#    print("내 이름은 홍길동",irum)
+    
+#for fruit in ["사과","배","복숭아","망고","멜론"]:
+#    print("I like",fruit)
+
+#for irum in range(0,51,2):
+#    print("내 이름은 홍길동",irum)
+
+#for i in range(10,0,-1):
+#    print(i)
+
+import time
+for tmr in range(5,0,-1):
+    time.sleep(tmr)
+    print("좋은날 되세요!!")
