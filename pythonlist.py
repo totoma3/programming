081e0650abdbc5815d64a79f1bf10dfdc836df2a
totoma3 <totoma3@naver.com>
2021-05-10 01:28:50 +0900

    Create pythonlist.py

---
+++ pythonlist.py
@@ -0,0 +1,14 @@
+mylist=[]
+mylist.append("사과")
+mylist.append("배")
+mylist.append("복숭아")
+mylist.append("포도")
+mylist.append("오렌지")
+print(mylist)
+print(mylist[0])
+print(mylist[1:3])
+print(mylist[2:])
+mylist.remove("사과")
+print(mylist)
+del mylist[0]
+print(mylist)
