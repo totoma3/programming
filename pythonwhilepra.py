e46193ca8b089ea29631b9e85c02ee47adef68c7
totoma3 <totoma3@naver.com>
2021-05-12 00:02:17 +0900

    Create pythonwhilepra.py

---
+++ pythonwhilepra.py
@@ -0,0 +1,23 @@
+#sum=0
+#i=0
+#while sum<=2000:
+#    i+=1
+#    if i<=100:
+#        sum+=i
+#print("2000초과 i=",i,"합계",sum)
+
+#sum=0
+#for i in range(1,101):
+#    if (sum>=2000):
+#        break
+#    else:
+#        sum+=i
+#print("합계 2000이 넘는 지점",i,"합계=",sum)
+
+while 1:
+    a=input("문자를 입력")
+    if a=="q":
+        break
+    print("입력한 문자는",a)
+print("프로그램을 종료합니다.")
+
