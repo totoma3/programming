c8af3e46e685cb5737229f6248ad1d1c3faed352
totoma3 <totoma3@naver.com>
2021-05-12 00:02:05 +0900

    Create pythonturtletest1.py

---
+++ pythonturtletest1.py
@@ -0,0 +1,18 @@
+import turtle
+t=turtle.Pen()
+t.forward(100)
+t.up()
+t.left(125)
+t.forward(100)
+t.down()
+t.backward(130)
+t.reset()
+t.left(45)
+t.forward(200)
+t.left(135)
+t.up()
+t.forward(150)
+t.left(135)
+t.down()
+t.forward(200)
+
