45b4e6a6b122a73b8f250b05178b9f3625e096e1
totoma3 <totoma3@naver.com>
2021-05-15 01:42:45 +0900

    Create testloop.py

---
+++ testloop.py
@@ -0,0 +1,8 @@
+gubun=["개나리","진달래","벚나무"]
+for irum in gubun:
+        tot=0
+        for i in range(2):
+            a=input("점수입력")
+            tot+=int(a)
+        print("반이름:",irum,"총점수:",tot)
+        
