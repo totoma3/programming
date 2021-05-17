966fcc6ac2005d009613f53b6609deee9d47679a
totoma3 <totoma3@naver.com>
2021-05-18 00:20:34 +0900

    Create python사용자정의함수.py

---
+++ python사용자정의함수.py
@@ -0,0 +1,12 @@
+def pline():
+    print("================",end="\n")
+
+
+print("원하시는 작업메뉴를 선택하세요")
+print("1. 포인트 적립")
+pline()
+print("2. 아이템 구입")
+pline()
+print("3. 아이템 팔기")
+pline()
+print("4. 포인트 조회")
